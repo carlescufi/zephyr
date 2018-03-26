47b5761f46fe80c7fb50f658593d872fc5e7921f
Carles Cufi <carles.cufi@nordicsemi.no>
2018-03-26 13:08:58 +0200

    cacu: print irq info in hello_world
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,19 @@
 
 #include <zephyr.h>
 #include <misc/printk.h>
+#include <soc.h>
+#include <arch/arm/cortex_m/exc.h>
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_ARCH);
+	printk("%s: %s\n", CONFIG_ARCH, CONFIG_BOARD);
+	
+#if defined(CONFIG_ZERO_LATENCY_IRQS)
+	printk("ZLI ON\n");
+#else
+	printk("ZLI OFF\n");
+#endif
+	printk("CONFIG_NUM_IRQ_PRIO_BITS: %d\n", CONFIG_NUM_IRQ_PRIO_BITS);
+	printk("_IRQ_PRIO_OFFSET: %d\n", _IRQ_PRIO_OFFSET);
+	printk("_EXC_IRQ_DEFAULT_PRIO: %d\n", _EXC_IRQ_DEFAULT_PRIO);
 }
