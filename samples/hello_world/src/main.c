060103504b994aa2ba1142cb2fe81b32c3b40941
Carles Cufi <carles.cufi@nordicsemi.no>
2019-02-21 16:27:15 +0100

    bbc:microbit sample for serial display
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,27 @@
  */
 
 #include <zephyr.h>
-#include <misc/printk.h>
+#include <uart.h>
+
+#define UART_DEVICE_NAME DT_UART_0_NAME
+struct device *uart_dev;
+
+const u8_t msg[] = "hello";
+
+void poll_out(void)
+{
+	int i;
+    	for (i = 0; i < ARRAY_SIZE(msg); i++) {
+        	uart_poll_out(uart_dev, msg[i]);
+    	}
+}
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+#ifdef CONFIG_PRINTK
+	printk("hello");
+#else
+	uart_dev = device_get_binding(UART_DEVICE_NAME);
+	poll_out();
+#endif
 }
